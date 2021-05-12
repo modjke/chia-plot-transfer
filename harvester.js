642d8550fdee6d216a219df1f3b7732f6aab8ba3
ignatiev <ignatiev.reg@gmail.com>
2021-05-12 17:42:15 +0300

    shit just got real

--- harvester.js
+++ harvester.js
@@ -1,10 +1,15 @@
 
-const { promises: fs, constants, stat } = require('fs')
+const fs = require('fs')
 const { promisify } = require('util')
 const path = require('path')
 const checkDiskUsage = promisify(require('diskusage').check)
 const http = require('http')
 const axios = require('axios').default
+const stream = require('stream')
+const createProgressStream = require('progress-stream')
+const exec = promisify(require('child_process').exec)
+
+
 
 void async function main() {
   console.log(`[Harvester] Starting up...`)
@@ -18,49 +23,145 @@ void async function main() {
 
   const farmers = config.farmers.map(url => new Farmer(url))
 
-  
+
   void async function fetchPlots() {
-    console.log(`Updating drives list...`)
     await driveManager.update()
 
-    try {
-      for (const farmer of farmers) {        
-        if (farmer.busy) continue
 
-        console.log(`Checking plots at ${farmer.url}...`)
+    for (const farmer of farmers) {
+      if (farmer.busy) continue
+
+      try {
         const plot = await farmer.getPlot()
 
         if (!plot) continue
 
+        console.log(`Found a plot to download: ${plot.name}`)
+
         const drivePath = await driveManager.getDrive(plot.size)
         if (!drivePath) {
-          console.error(`NO SPACE LEFT`)
+          console.error(`ERROR: NO SPACE LEFT`)
         }
-        const plotPath = await downloadPlot(farmer, plot, drivePath)
-        const valid = await validatePlot(plotPath)
-      }
-    } catch (error) {
-      console.log(error)
-    }
 
-    // setTimeout(fetchPlots, 60000)
+        const plotPath = await download(plot, drivePath)
+        const valid = await chiaValidatePlot(config.chiaDir, plotPath)
+        if (valid) {
+          await farmer.remove(plot)
+        } else {
+          silentRm(plotPath)
+          console.error(`ERROR: INVALID PLOT!!!`, plotPath)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+
+
+      setTimeout(fetchPlots, 60000)
+    }
   }()
+
 }()
 
+function loadConfig() {
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, 'harvester.json'), 'utf-8'))
+}
 
-async function loadConfig() {
-  return JSON.parse(await fs.readFile(path.resolve(__dirname, 'harvester.json'), 'utf-8'))
+
+async function chiaValidatePlot(cwd, plot) {
+  try {
+    const shell = `/bin/bash`
+    const { stderr = '' } = await exec(`${shell} -c 'cd ${cwd}; . ./activate; chia plots check -g ${plot};'`)
+    const match = /Found\s(\d+)\svalid\splots/g.exec(stderr)
+    const validPlotCount = (match || [])[1] || 0
+
+    if (validPlotCount > 1) {
+      console.error(`ERROR: more than one valid plot returned by chiaValidate`)
+    }
+
+    return validPlotCount === 1
+  } catch (error) {
+    console.log(`it's ok if you are on windows`, error)
+    return 0
+  }
+
+}
+
+async function download({ downloadUrl, name, size }, destDir) {
+  return new Promise((resolve, reject) => {
+    const dstfname = path.join(destDir, name)
+
+    if (fs.existsSync(dstfname)) {
+      return reject(new Error(`dest file already exists...`))
+    }
+
+    const tmpfname = path.join(destDir, name + '.tmp')
+
+    console.log(`Downloading plot to ${tmpfname}...`)
+
+
+    http.get(downloadUrl, response => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Non 200 status`))
+        return
+      }
+
+      const progress = createProgressStream({
+        length: size,
+        time: 10000
+      })
+
+      progress.on('progress', progress => {
+        console.log(`Percentage: ${progress.percentage}%, ETA: ${(progress.eta / 60).toFixed(1)} min`)
+      })
+
+      stream.pipeline(
+        response,
+        progress,
+        fs.createWriteStream(tmpfname),
+        err => {
+          if (err) {
+            console.error(err)
+
+            silentRm(tmpfname)
+
+            reject(new Error(`Stream ended with error`))
+          } else {
+            try {
+              console.log(`Renaming plot ot ${dstfname}...`)
+              fs.renameSync(tmpfname, dstfname)
+
+              resolve(dstfname)
+            } catch (error) {
+              silentRm(tmpfname)
+              silentRm(dstfname)
+
+              reject(new Error(`Unable to rename tmp file into .plot`))
+            }
+          }
+        }
+      )
+    })
+  })
+
+}
+
+function silentRm(filepath) {
+  try {
+    fs.unlinkSync(filepath)
+  } catch (ignore) {
+
+  }
 }
 class DriveManager {
   constructor(parentMount) {
-    this.parentMount = path.resolve(__dirname, parentMount)    
+    this.parentMount = path.resolve(__dirname, parentMount)
   }
 
   async update() {
-    const entries = (await fs.readdir(this.parentMount)).map(entry => path.join(this.parentMount, entry))
+    const entries = fs.readdirSync(this.parentMount).map(entry => path.join(this.parentMount, entry))
     const drivePaths = []
-    const dirContentStats = await Promise.all(entries.map(entry => fs.stat(entry)))
-    
+    const dirContentStats = entries.map(entry => fs.statSync(entry))
+
     for (let i = 0; i < dirContentStats.length; i++) {
       const stats = dirContentStats[i]
       const entry = entries[i]
@@ -71,7 +172,7 @@ class DriveManager {
 
     const drivesFreeSpaces = await Promise.all(drivePaths.map(p => checkDiskUsage(p)))
 
-    
+
     this.drives = drivePaths.map((p, i) => {
       const bytes = drivesFreeSpaces[i]
 
@@ -81,12 +182,12 @@ class DriveManager {
       }
     })
 
-    
+
   }
 
   getDrive(size) {
     const drive = this.drives.find(drive => {
-      return drive.bytes.available <= size
+      return drive.bytes.available > size
     })
 
     if (!drive) {
@@ -107,7 +208,7 @@ class Farmer {
     this.busy = false
   }
 
-  async getPlot() {    
+  async getPlot() {
     try {
       const res = await axios.get(this.url)
       if (!Array.isArray(res.data)) {
@@ -118,25 +219,27 @@ class Farmer {
         const [name] = res.data
 
         const downloadUrl = this.url + 'download/' + name
-        
-        const { headers: { 'content-length': contentLength } }= await axios.head(downloadUrl)
+
+        const { headers: { 'content-length': contentLength } } = await axios.head(downloadUrl)
         const size = parseInt(contentLength)
         if (isNaN(size)) {
           throw new Error(`Invalid content-length`, contentLength)
         }
 
-        return { 
+        return {
           downloadUrl,
           size,
           name
         }
       }
     } catch (error) {
-      console.error(error)      
+      console.error(error)
     }
 
     return null;
   }
 
+  async remove({ name }) {
+    await axios.get(this.url + 'remove/' + name)    
+  } 
 }
- 
